9814a49385b7cd73d4f39150188225e4bf8bbf7e
Denys Zaluzhnyi <denys.zaluzhnyi@gmail.com>
2023-05-30 09:53:25 +0300

    test: Add toast-icon

--- src/notifier/work_notifier.py
+++ src/notifier/work_notifier.py
@@ -1,3 +1,6 @@
+import os.path
+from os.path import join
+
 from pynotifier import NotificationClient, Notification
 from pynotifier.backends import platform
 
@@ -9,8 +12,11 @@ class WorkNotifier():
 
     def send(self, message: str) -> None:
         print('Work Concentrator: ', message)
+        icon_path = os.path.abspath(join('.', 'media', 'icon.ico'))
+        print(icon_path)
         notification = Notification(
             title='Work Scheduler',
             message=message,
+            icon_path=icon_path,
         )
         self.notify_client.notify_all(notification)
